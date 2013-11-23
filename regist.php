1c0d0373ce1890ed1b3dc38b9b33cf7b950be0e6
takesi-nakao <takesi.nakao@gmail.com>
2013-11-23 15:05:19 +0900

    regist.php
    add regist.php

---
+++ regist.php
@@ -0,0 +1,59 @@
+<?php
+function enc($str, $charset = 'UTF-8') {
+	print(htmlspecialchars($str, ENT_QUOTES, $charset));
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>PapaPaper - Registration Result</title>
+</head>
+<body>
+
+<?php
+    // DB connection info
+    //TODO: Update the values for $host, $user, $pwd, and $db
+    //using the values you retrieved earlier from the portal.
+/*
+Database=pppp;Data Source=ap-cdbr-azure-east-b.cloudapp.net;User Id=b623d2a9e26e87;Password=fa4ba3e9
+*/
+    $host = "ap-cdbr-azure-east-b.cloudapp.net";
+    $user = "b623d2a9e26e87";
+    $pwd = "fa4ba3e9";
+    $db = "pppp";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    // Insert registration info
+    if(!empty($_POST)) {
+    try {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $date = date("Y-m-d");
+        // Insert data
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
+        $stmt->execute();
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+    }
+?>
+
+<h1>登録ありがとうございました</h1>
+<p><?php print($_POST['name']); ?></p>
+<p><?php print($*POST['email']); ?></p>
+
+</body>
+</html>
