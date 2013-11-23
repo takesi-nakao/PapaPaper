482932b13dada7567f7d5f320969cc9ea16678cf
takesi-nakao <takesi.nakao@gmail.com>
2013-11-23 14:45:05 +0900

    あ
    あ

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+//require_once 'encode.php';
 function e($str, $charset = 'UTF-8') {
 	print(htmlspecialchars($str, ENT_QUOTES, $charset));
 }
+//session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,6 +18,7 @@ function e($str, $charset = 'UTF-8') {
 <div class="container">
 	<label for="name">お名前：</label><br/>
 	<input type="text" name="name" id="name" value=""/>
+	<input type="text" name="name" id="name" value="<?php print(e($_SESSION['name'])); ?>"/>
 </div>
 <div class="container">
 	<label for="email">メールアドレス：</label><br/>
@@ -23,64 +26,5 @@ function e($str, $charset = 'UTF-8') {
 </div>
 <input type="submit" name="submit" value="登録" />
 </form>
-<?php
-/*
-Database=pppp;Data Source=ap-cdbr-azure-east-b.cloudapp.net;User Id=b623d2a9e26e87;Password=fa4ba3e9
-*/
-    // DB connection info
-    //TODO: Update the values for $host, $user, $pwd, and $db
-    //using the values you retrieved earlier from the portal.
-	$host = "ap-cdbr-azure-east-b.cloudapp.net";
-    $user = "b623d2a9e26e87";
-    $pwd = "fa4ba3e9";
-    $db = "pppp";
-    // Connect to database.
-    try {
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch(Exception $e){
-        die(var_dump($e));
-    }
-    // Insert registration info
-    if(!empty($_POST)) {
-    try {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $date = date("Y-m-d");
-        // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $date);
-        $stmt->execute();
-    }
-    catch(Exception $e) {
-        die(var_dump($e));
-    }
-    echo "<h3>Your're registered!</h3>";
-    }
-    // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll(); 
-    if(count($registrants) > 0) {
-        echo "<h2>People who are registered:</h2>";
-        echo "<table>";
-        echo "<tr><th>Name</th>";
-        echo "<th>Email</th>";
-        echo "<th>Date</th></tr>";
-        foreach($registrants as $registrant) {
-            echo "<tr><td>".$registrant['name']."</td>";
-            echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "<h3>No one is currently registered.</h3>";
-    }
-?>
 </body>
 </html>
