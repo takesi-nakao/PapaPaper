b3a4e185744c96efcbeeeda1625dc2f64224c3eb
takesi-nakao <takesi.nakao@gmail.com>
2013-11-24 08:13:47 +0900

    test regist new page
    新しい登録ページのテストアップ

---
+++ _pretest/index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>パパペパ！　パパのためのイクメン情報サービス</title>
+<link rel="stylesheet" type="text/css" href="css/cssreset.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/cssfonts.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/default.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+</head>
+<body>
+<div id="wrap_all">
+    <header>
+        <div id="logo_head"><img src="images/logo_head.png" width="509" height="259" alt="パパペパ"></div>
+        <form  method="post" action="regist.php" enctype="multipart/form-data" >
+        <div id="form_head">
+            <p id="mail">メール
+                <input id="text_mail_head" type="text" name="example2" maxlength="30">
+            </p>
+            <input id="submit_head" type="image" src="images/btn.png" alt="登録" >
+            <p id="comment">＜システム稼働の準備中＞<br />
+                事前登録の状態のため、登録確認メールは送信されません。</p>
+        </div>
+		</form>
+    </header>
+    <div id="contents">
+        <div id="contents_what">
+            <dl>
+                <dt class="title" id="title_what"> 【PapaPaper(パパペパ)とは…】 </dt>
+                <dd>
+                    <p class="tx"> パパに役立つ情報を、パパの代わりに、<br />
+                        インターネット上で「探して」「まとめて」お届けするサービスです。<br />
+                    </p>
+                    <ul class="tx">
+                        <li>・妊娠中のママにしてあげると良いこと</li>
+                        <li>・０歳の赤ちゃんにしてあげると喜ぶこと</li>
+                        <li>・お住まいの地域のセミナー情報　　など</li>
+                        <li class="tx_s">※現時点では、ママが妊娠中、または子どもが０～３歳のパパを想定しています。</li>
+                    </ul>
+                </dd>
+            </dl>
+        </div>
+        <div id="contents_kankyou">
+            <dl>
+                <dt class="title" id="title_kankyou"> 【現在のパパを取り巻く環境】 </dt>
+                <dd>
+                    <p class="tx_2"> 共働き夫婦の増加、パパへの子育て参加希望の声<br />
+                        イクメンという言葉の誕生など<br />
+                        パパを取り巻く環境は変化してきています。<br />
+                        <br />
+                        今では、官公庁、NPO、民間企業、個人まで<br />
+                        様々な人がパパ向けの育児情報を発信しています。<br />
+                        <br />
+                        でも、パパに役立つ情報は、家族によってバラバラ。<br />
+                        <br />
+                        ママは妊娠２か月なのか？６か月なのか？<br />
+                        子どもは０歳なのか？２歳なのか？<br />
+                        <br />
+                        そんな状況の違いでも、役立つ情報は変わります。<br />
+                        <br />
+                        情報は増えましたが、自分たちにマッチした情報を得るのは<br />
+                        難しくて手間がかかるのが現状です。 </p>
+                </dd>
+            </dl>
+        </div>
+        <div id="contents_mokuhyou">
+            <dl>
+                <dt class="title" id="title_mokuhyou"> 【PapaPaper(パパペパ)が目指すもの】 </dt>
+                <dd class="tx"> ママが妊娠何か月なのか？<br />
+                    子どもは何歳なのか？<br />
+                    住んでいるエリアは？<br />
+                    <br />
+                    そういった情報を登録するだけで、<br />
+                    PapaPaperは、役に立つ情報をインターネット上で<br />
+                    「探して」「まとめて」お届けします。<br />
+                    <br />
+                    ママや子どもに優しくて楽しませられる。<br />
+                    そんなパパを１人でも多く増やしたいと考えています。 </dd>
+            </dl>
+        </div>
+    </div>
+    <footer>
+        <div id="logo_foot"><img src="images/logo_foot.png" width="493" height="249" alt="パパペパ"></div>
+        <div id="form_foot"> <a href="#wrap_all" id="#wrap_all"><img src="images/btn.png" width="500" height="32" alt=""></a> </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ _pretest/regist.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>パパペパ！　パパのためのイクメン情報サイト</title>
+<link rel="stylesheet" type="text/css" href="css/cssreset.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/cssfonts.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/default.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+</head>
+<body>
+<div id="wrap_all">
+    <header>
+        <div id="logo_head"><img src="images/logo_head.png" width="509" height="259" alt="パパペパ"></div>
+        <p id="orei">ご登録ありがとうございました！</p>
+    </header>
+</div>
+
+<?php
+    // DB connection info
+    //TODO: Update the values for $host, $user, $pwd, and $db
+    //using the values you retrieved earlier from the portal.
+/*
+Database=pppp;Data Source=ap-cdbr-azure-east-b.cloudapp.net;User Id=b623d2a9e26e87;Password=fa4ba3e9
+*/
+    $host = "ap-cdbr-azure-east-b.cloudapp.net";
+    $user = "b623d2a9e26e87";
+    $pwd = "fa4ba3e9";
+    $db = "pppp";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        exit(var_dump($e));
+    }
+	// Insert registration info
+	if(!empty($_POST)) {
+		try {
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$date = date("Y-m-d");
+			// Insert data
+			$sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+				   VALUES (?,?,?)";
+			$stmt = $conn->prepare($sql_insert);
+			$stmt->bindValue(1, $name);
+			$stmt->bindValue(2, $email);
+			$stmt->bindValue(3, $date);
+			$stmt->execute();
+		}
+		catch(Exception $e) {
+			exit(var_dump($e));
+		}
+	}
+
+?>
+
+<!--
+<p><?php print('お名前：'.$_POST['name']); ?></p>
+<p><?php print('メールアドレス：'.$_POST['email']); ?></p>
+-->
+
+</body>
+</html>
