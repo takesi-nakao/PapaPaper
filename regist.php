e7775fbe4e70b184e5fac2e3b87670004f2ac180
takesi-nakao <takesi.nakao@gmail.com>
2013-11-23 16:54:39 +0900

    a
    a

--- regist.php
+++ regist.php
@@ -38,21 +38,22 @@ Database=pppp;Data Source=ap-cdbr-azure-east-b.cloudapp.net;User Id=b623d2a9e26e
     }
 	// Insert registration info
 	if(!empty($_POST)) {
-	try {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$date = date("Y-m-d");
-		// Insert data
-		$sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+		try {
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$date = date("Y-m-d");
+			// Insert data
+			$sql_insert = "INSERT INTO registration_tbl (name, email, date) 
 				   VALUES (?,?,?)";
-		$stmt = $conn->prepare($sql_insert);
-		$stmt->bindValue(1, $name);
-		$stmt->bindValue(2, $email);
-		$stmt->bindValue(3, $date);
-		$stmt->execute();
-	}
-	catch(Exception $e) {
-		exit(var_dump($e));
+			$stmt = $conn->prepare($sql_insert);
+			$stmt->bindValue(1, $name);
+			$stmt->bindValue(2, $email);
+			$stmt->bindValue(3, $date);
+			$stmt->execute();
+		}
+		catch(Exception $e) {
+			exit(var_dump($e));
+		}
 	}
 
 ?>
